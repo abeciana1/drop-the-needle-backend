19d74c4c9d7c7e8d2a699510ea2d9c53a07f83d3
Alex <alex.beciana@gmail.com>
2022-07-14 23:32:28 -0400

    Added creare, update, and delete actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,47 @@ class Api::V1::UsersController < ApplicationController
         render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
 
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            # render json: { user: UserSerializer.new(user) }, status: :created
+            token = encode_token(user_id: user.id)
+            cookies.signed[:jwt] = {value: token, httponly: true,   expires: 1.hour.from_now}
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+        else
+            #! look into custom error messaging
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    # ! Try again later
+    # def check_email
+    #     if user_params[:email].blank?
+    #         return render json: {error: 'Email not present'}
+    #     end
+
+    #     user = User.find_by(email: user_params[:email])
+    #     if user.valid?
+    #         user.generate_password_token!
+    #         #! SEND EMAIL HERE via sendgrid
+    #         SendGrid.send_forgot_password(user.email, user,"d-302fe0775770410faf7d59800cbc8d34")
+    #         render json: {user: UserSerializer.new(user)}, status: :accepted
+    #     else
+    #         render json: { error: 'failed to find user' }, status: :not_acceptable
+    #     end
+    # end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render :json => user, each_serializer: UserSerializer
+    end
     
+    def delete
+        user = User.find(params[:id])
+        user.destroy
+        render json: {}
+    end
 
     private
 
