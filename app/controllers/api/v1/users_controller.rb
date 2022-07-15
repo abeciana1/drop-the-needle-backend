e0a5ded8950bdefabb8030fa012c881cb47428cc
Alex <alex.beciana@gmail.com>
2022-07-14 23:29:27 -0400

    Added user params method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,4 +26,16 @@ class Api::V1::UsersController < ApplicationController
         }
         render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
+
+    
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :email,
+            :password
+        )
+    end
 end
