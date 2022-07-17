5505fe1f063996b866b3bd649b4fd54581cd1b30
Alex <alex.beciana@gmail.com>
2022-07-17 12:32:31 -0400

    Created power hour and host seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,5 +40,39 @@ fake3 = User.create(
 
 puts 'create fake power hours'
 puts 'fake power hour 1'
+ph1 = PowerHour.create(
+    title: 'fake power hour 1',
+    description: Faker::Hipster.sentence,
+    cover_image: 'https://www.thebeatles.com/sites/default/files/styles/responsive_thumbnail_mobile/public/2021-06/Magical-Mystery-Tour.jpg?itok=8midCi2f'
+    date_time: Faker::Date.between(from: '2022-08-01', to: '2022-08-31')
+)
+puts 'fake power hour 2'
+ph2 = PowerHour.create(
+    title: 'fake power hour 2',
+    description: Faker::Hipster.sentence,
+    cover_image: 'https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg'
+    date_time: Faker::Date.between(from: '2022-08-01', to: '2022-08-31')
+)
+puts 'fake power hour 3'
+ph3 = PowerHour.create(
+    title: 'fake power hour 3',
+    description: Faker::Hipster.sentence,
+    cover_image: 'https://www.cleveland.com/resizer/QXH4tEN4nFdZgqSSPz0nPIyuk7k=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/2CJOQE4IK5AY5OD33OHU5WOIIU.jpg'
+    date_time: Faker::Date.between(from: '2022-08-01', to: '2022-08-31')
+)
+
+puts 'Create hosts'
+h1 = Host.create(
+    user_id: alex.id,
+    power_hour: ph1.id
+)
+h2 = Host.create(
+    user_id: fake1.id
+    power_hour: ph2.id
+)
+h3 = Host.create(
+    user_id: fake2.id,
+    power_hour: ph3.id
+)
 
 0
