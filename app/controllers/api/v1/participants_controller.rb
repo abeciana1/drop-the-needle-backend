37d4b9f63d74f77ce0debad3d675eb4da98fe004
Alex <alex.beciana@gmail.com>
2022-07-16 22:50:42 -0400

    Added participant controller methods

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -1,2 +1,35 @@
+require 'pry'
+
 class ParticipantsController < ApplicationController
+
+    def create
+        binding.pry
+        participant = Participant.create(participant_params)
+        if participant.valid?
+            render json: { participant: ParticipantSerializer.new(participant) }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    def update
+        participant = Participant.find(params[:id])
+        participant.update(participant_params)
+        render :json => participant, each_serializer: ParticipantSerializer
+    end
+    
+    def delete
+        participant = Participant.find(params[:id])
+        participant.destroy
+        render json: {}
+    end
+
+    private
+
+    def participant_params
+        params.permit(
+            :user_id,
+            :power_hour_id
+        )
+    end
 end
