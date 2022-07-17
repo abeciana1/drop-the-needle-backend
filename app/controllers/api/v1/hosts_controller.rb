d6a293d69f627a9c4baa2d52a7781846ce40aacb
Alex <alex.beciana@gmail.com>
2022-07-16 22:40:58 -0400

    Created host controller create method

--- app/controllers/api/v1/hosts_controller.rb
+++ app/controllers/api/v1/hosts_controller.rb
@@ -1,2 +1,21 @@
+require 'pry'
+
 class HostsController < ApplicationController
+
+    def create
+        binding.pry
+        host = Host.create(host_params)
+        if host.valid?
+            render json: { host: HostSerializer.new(host) }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    params.permit(
+        :user_id,
+        :power_hour_id
+    )
 end
