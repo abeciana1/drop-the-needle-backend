c5cb017837f3830953fa9524270a2f0dcec113d7
Alex <alex.beciana@gmail.com>
2022-07-16 22:15:01 -0400

    Added fields and relationships to user and song serializers:

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,3 +1,6 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :artist, :album, :youtube_link, :start_time, :end_time, :power_hour_id, :order_number, :user_id
+
+  has_one :user
+  has_many :power_hour
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email
+
+  has_many :participants
+  has_many :hosts
 end
