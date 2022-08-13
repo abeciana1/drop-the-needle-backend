e2fcbbee86a81a37eaa3b2f64446fef59c56c26e
Alex <alex.beciana@gmail.com>
2022-08-13 19:49:01 -0400

    Added song embed_link method to serializer adding logic to server side -- removed on client

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,4 +5,21 @@ class Song < ApplicationRecord
     def user
         User.find(self.user_id).name
     end
+
+    def time_to_second_convert(time)
+        split_time = time.split(':').map(&:to_i)
+        if split_time.length === 3
+            (split_time[0]) * 60 * 60 + (split_time[1]) * 60 + (split_time[2])
+        else
+            (split_time[0]) * 60 + (split_time[1])
+        end
+    end
+
+    def embed_link
+        converted_start_time = time_to_second_convert(self.start_time)
+        converted_end_time = time_to_second_convert(self.end_time)
+        # binding.pry
+        embed_link = "#{self.youtube_link}?start=#{converted_start_time}&end=#{converted_end_time}"
+        return embed_link
+    end
 end
