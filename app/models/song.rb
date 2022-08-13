3f5389539a3dc7d3c12cdb1ccb962dfea60b04a6
Alex <alex.beciana@gmail.com>
2022-08-12 23:43:45 -0400

    Added method for song instance to return user name

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,8 @@
 class Song < ApplicationRecord
     belongs_to :user
     belongs_to :power_hour
+
+    def user
+        User.find(self.user_id).name
+    end
 end
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,6 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :title, :artist, :album, :youtube_link, :start_time, :end_time, :power_hour_id, :order_number, :user_id
+  attributes :id, :title, :artist, :album, :youtube_link, :start_time, :end_time, :power_hour_id, :order_number, :user
 
-  has_one :user
   has_many :power_hour
 end
