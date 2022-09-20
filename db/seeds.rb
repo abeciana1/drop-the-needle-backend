4190caaab7eae82f026244fed61c197eb20d75ac
Alex <alex.beciana@gmail.com>
2022-09-20 14:49:46 -0400

    Commented seed data back in

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,105 +8,104 @@
 require 'pry'
 require 'faker'
 
-# puts "destroy all"
-# User.destroy_all
-# Host.destroy_all
-# Participant.destroy_all
+puts "destroy all"
+User.destroy_all
+Host.destroy_all
+Participant.destroy_all
 Song.destroy_all
-# PowerHour.destroy_all
+PowerHour.destroy_all
 
-# puts 'create testing account'
-# alex = User.create(name: 'Alex', email: 'alex@test.com', password: '1234')
+puts 'create testing account'
+alex = User.create(name: 'Alex', email: 'alex@test.com', password: '1234')
 
-# puts 'create fake users'
-# puts 'fake user 1'
-# fake1 = User.create(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     password: '5678'
-# )
-# puts 'fake user 2'
-# fake2 = User.create(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     password: '5678'
-# )
-# puts 'fake user 3'
-# fake3 = User.create(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     password: '5678'
-# )
+puts 'create fake users'
+puts 'fake user 1'
+fake1 = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: '5678'
+)
+puts 'fake user 2'
+fake2 = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: '5678'
+)
+puts 'fake user 3'
+fake3 = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: '5678'
+)
 
-# puts 'create fake power hours'
-# puts 'fake power hour 1'
-# ph1 = PowerHour.create(
-#     title: 'fake power hour 1',
-#     description: Faker::Hipster.sentence,
-#     cover_image: 'https://www.thebeatles.com/sites/default/files/styles/responsive_thumbnail_mobile/public/2021-06/Magical-Mystery-Tour.jpg?itok=8midCi2f',
-#     date_time: Faker::Date.between(from: '2022-10-01', to: '2022-10-31'),
-#     submission_deadline: Faker::Date.between(from: '2022-10-11', to: '2022-10-31'),
-#     publish_status: false
-# )
-# puts 'fake power hour 2'
-# ph2 = PowerHour.create(
-#     title: 'fake power hour 2',
-#     description: Faker::Hipster.sentence,
-#     cover_image: 'https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg',
-#     date_time: Faker::Date.between(from: '2022-10-01', to: '2022-10-31'),
-#     submission_deadline: Faker::Date.between(from: '2022-10-11', to: '2022-10-31'),
-#     publish_status: false
-# )
-# puts 'fake power hour 3'
-# ph3 = PowerHour.create(
-#     title: 'fake power hour 3',
-#     description: Faker::Hipster.sentence,
-#     cover_image: 'https://www.cleveland.com/resizer/QXH4tEN4nFdZgqSSPz0nPIyuk7k=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/2CJOQE4IK5AY5OD33OHU5WOIIU.jpg',
-#     date_time: Faker::Date.between(from: '2022-10-01', to: '2022-10-31'),
-#     submission_deadline: Faker::Date.between(from: '2022-10-11', to: '2022-10-31'),
-#     publish_status: false
-# )
+puts 'create fake power hours'
+puts 'fake power hour 1'
+ph1 = PowerHour.create(
+    title: 'fake power hour 1',
+    description: Faker::Hipster.sentence,
+    cover_image: 'https://www.thebeatles.com/sites/default/files/styles/responsive_thumbnail_mobile/public/2021-06/Magical-Mystery-Tour.jpg?itok=8midCi2f',
+    date_time: Faker::Date.between(from: '2022-10-01', to: '2022-10-31'),
+    submission_deadline: Faker::Date.between(from: '2022-10-11', to: '2022-10-31'),
+    publish_status: false
+)
+puts 'fake power hour 2'
+ph2 = PowerHour.create(
+    title: 'fake power hour 2',
+    description: Faker::Hipster.sentence,
+    cover_image: 'https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg',
+    date_time: Faker::Date.between(from: '2022-10-01', to: '2022-10-31'),
+    submission_deadline: Faker::Date.between(from: '2022-10-11', to: '2022-10-31'),
+    publish_status: false
+)
+puts 'fake power hour 3'
+ph3 = PowerHour.create(
+    title: 'fake power hour 3',
+    description: Faker::Hipster.sentence,
+    cover_image: 'https://www.cleveland.com/resizer/QXH4tEN4nFdZgqSSPz0nPIyuk7k=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/2CJOQE4IK5AY5OD33OHU5WOIIU.jpg',
+    date_time: Faker::Date.between(from: '2022-10-01', to: '2022-10-31'),
+    submission_deadline: Faker::Date.between(from: '2022-10-11', to: '2022-10-31'),
+    publish_status: false
+)
 
-# puts 'Create hosts'
-# h1 = Host.create(
-#     user_id: alex.id,
-#     power_hour_id: ph1.id
-# )
-# h2 = Host.create(
-#     user_id: fake1.id,
-#     power_hour_id: ph2.id
-# )
-# h3 = Host.create(
-#     user_id: fake2.id,
-#     power_hour_id: ph3.id
-# )
+puts 'Create hosts'
+h1 = Host.create(
+    user_id: alex.id,
+    power_hour_id: ph1.id
+)
+h2 = Host.create(
+    user_id: fake1.id,
+    power_hour_id: ph2.id
+)
+h3 = Host.create(
+    user_id: fake2.id,
+    power_hour_id: ph3.id
+)
 
-# puts 'Create participants for power hours'
-# puts 'power hour 1'
-# php1 = Participant.create(
-#     user_id: alex.id,
-#     power_hour_id: ph1.id,
-#     confirmed_rsvp: true
-# )
-# php2 = Participant.create(
-#     user_id: fake1.id,
-#     power_hour_id: ph1.id,
-#     confirmed_rsvp: false
-# )
-# php3 = Participant.create(
-#     user_id: fake2.id,
-#     power_hour_id: ph1.id,
-#     confirmed_rsvp: false
-# )
-# php4 = Participant.create(
-#     user_id: fake3.id,
-#     power_hour_id: ph1.id,
-#     confirmed_rsvp: false
-# )
+puts 'Create participants for power hours'
+puts 'power hour 1'
+php1 = Participant.create(
+    user_id: alex.id,
+    power_hour_id: ph1.id,
+    confirmed_rsvp: true
+)
+php2 = Participant.create(
+    user_id: fake1.id,
+    power_hour_id: ph1.id,
+    confirmed_rsvp: false
+)
+php3 = Participant.create(
+    user_id: fake2.id,
+    power_hour_id: ph1.id,
+    confirmed_rsvp: false
+)
+php4 = Participant.create(
+    user_id: fake3.id,
+    power_hour_id: ph1.id,
+    confirmed_rsvp: false
+)
 
 puts 'Create songs'
 puts 'Create song 1'
-# binding.pry
 s1 = Song.create(
     title: Faker::Music::RockBand.song,
     artist: Faker::Music::RockBand.name,
@@ -114,8 +113,8 @@ s1 = Song.create(
     youtube_link: 'https://www.youtube.com/watch?v=W8r-tXRLazs',
     start_time: '0:10',
     end_time: '0:15',
-    power_hour_id: PowerHour.first.id,
-    user_id: User.first.id,
+    power_hour_id: ph1.id,
+    user_id: alex.id,
     order_number: 1)
 puts 'Create song 2'
 s2 = Song.create(
@@ -125,8 +124,8 @@ s2 = Song.create(
     youtube_link: 'https://www.youtube.com/watch?v=uhG-vLZrb-g',
     start_time: '0:10',
     end_time: '0:15',
-    power_hour_id: PowerHour.first.id,
-    user_id: User.all.sample.id,
+    power_hour_id: ph1.id,
+    user_id: fake1.id,
     order_number: 2
 )
 puts 'Create song 3'
@@ -137,8 +136,8 @@ s3 = Song.create(
     youtube_link: 'https://www.youtube.com/watch?v=wVrSXeRXSWE',
     start_time: '0:10',
     end_time: '0:15',
-    power_hour_id: PowerHour.first.id,
-    user_id: User.all.sample.id,
+    power_hour_id: ph1.id,
+    user_id: fake2.id,
     order_number: 3
 )
 puts 'Create song 4'
@@ -149,8 +148,8 @@ s3 = Song.create(
     youtube_link: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
     start_time: '0:10',
     end_time: '0:15',
-    power_hour_id: PowerHour.first.id,
-    user_id: User.all.sample.id,
+    power_hour_id: ph1.id,
+    user_id: fake3.id,
     order_number: 4
 )
 
