ac7940ecf4059962cd3d2c87ac45a0ece2760251
Alex <alex.beciana@gmail.com>
2022-09-20 14:48:35 -0400

    Updated song instance method - creation issue

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ApplicationRecord
     belongs_to :user
     belongs_to :power_hour
 
-    def user
+    def user_name
         User.find(self.user_id).name
     end
 
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,5 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :title, :artist, :album, :youtube_link, :start_time, :end_time, :power_hour_id, :order_number, :user, :embed_link
+  attributes :id, :title, :artist, :album, :youtube_link, :start_time, :end_time, :power_hour_id, :order_number, :user_name, :embed_link
 
   has_many :power_hour
 end
